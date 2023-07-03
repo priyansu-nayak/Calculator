a91dd710e13801d1d7035661fed127e582345958
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-03 08:40:28 +0530

    New Update

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-/*
-data attributes is used in javascript instead of classes
-bcoz it's easier to see what parts of HTML are being used by
-JavaScript and which parts are used by actual CSS.
-*/
-
-
 class Calculator{
-    constructor(previousOperand,currentOperand){
-        this.previousOperand=previousOperand
-        this.currentOperand=currentOperand
+    constructor(previousOperandTextElement,currentOperandTextElement){
+        this.previousOperandTextElement=previousOperandTextElement
+        this.currentOperandTextElement=currentOperandTextElement
+        //as soon as we refresh 
         this.clear()
+
     }
 
     clear(){
-        this.currentOperand=''
-        this.previousOperand=''
-        this.operation= undefined
+        this.currentOperand = ""
+        this.previousOperand =""
+        this.operation = undefined
     }
 
     delete(){
@@ -23,41 +18,40 @@ class Calculator{
     }
 
     appendNumber(number){
+        this.currentOperand=number
 
     }
-
-    chooseOperation(operation){
+    
+    chooseOpertion(operation){
 
     }
-
+    
     compute(){
 
     }
 
     updateDisplay(){
-
+        this.currentOperandTextElement.innerText = this.currentOperand
+        console.log(this.currentOperand)
     }
 
 }
 
+const numberButtons = document.querySelectorAll("[data-number]")
+const operationButtons = document.querySelectorAll("[data-operation]")
 
+const equalsButton = document.querySelector("[data-equals]")
+const deleteButton = document.querySelector("[data-delete]")
+const allClearButton = document.querySelector("[data-all-clear]")
+const previousOperandTextElement = document.querySelector("[data-previous-operand]")
+const currentOperandTextElement = document.querySelector("[data-current-operand]")
 
+const calculator= new Calculator(previousOperandTextElement,currentOperandTextElement)
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons= document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperand = document.querySelector('[data-previous-operand]')
-const currentOperand = document.querySelector('[data-current-operand]')
-
-
-const calculator = new Calculator(previousOperand, currentOperand)
 numberButtons.forEach(button =>{
-    button.addEventListener('click',()=>{
+    button.addEventListener("click",()=>{
         calculator.appendNumber(button.innerText)
+        
         calculator.updateDisplay()
     })
 })
-
-resume @ 20:42
